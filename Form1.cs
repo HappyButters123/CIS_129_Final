9c8749f388bf024a6db0d48f8ce72c8dd36cddab
Dex <dexman1223@gmail.com>
2024-10-08 13:35:34 -0400

    blank agian

--- Form1.cs
+++ Form1.cs
@@ -12,47 +12,9 @@ namespace CIS_129_Final
 {
     public partial class Form1 : Form
     {
-        string Answer = "";
-
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //changes text on button
-            if(Answer  == radioButton2.Text) {
-                button1.Text = "Right";
-            }
-            else {
-                button1.Text = "Wrong";
-            }
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            Answer = radioButton1.Text;
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            Answer = radioButton2.Text;
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            Answer = radioButton3.Text;
-        }
-
-        private void radioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-            Answer = radioButton4.Text;
-        }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-            richTextBox1.Text = "What color is the sky?";
         }
     }
 }
